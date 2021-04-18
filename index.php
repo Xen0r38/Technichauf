39c9d5c982334b0a1cfb9a18e49a3e673600a605
Xen0r38 <65468498+Xen0r38@users.noreply.github.com>
2021-04-18 14:29:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+
+<html lang="en" dir="ltr">
+
+
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css"/>
+    <meta name="description" content="TECHNI'CHAUF intervient dans un rayon de 30 kms autour de Les Avenières en Isère (38),
+    en Savoie et dans l'Ain et couvre les communes suivantes: Les communes de l 'Isère (38)"/>
+    <title>Techni'chauf</title>
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <!-- Images en haut du  site, logo et numero telephne -->
+        <img id="logoTechnichauf" src="Images/logo-technichauf.jpg" alt="logoTechnichauf">
+        <img width="16%" src="Images/technichauf-tel.jpg" alt="06 82 58 98 80">
+      </div>
+
+      <nav>
+         <a class="lienNav" href="index.html"> Accueil</a>  |
+         <a class="lienNav" href="contact.html"> Contact </a>
+      </nav>
+    </header>
+
+    <section>
+      <div class="container">
+        <img width="100%" height="400px" src="Images/granulé.JPG">
+
+        <article>
+          <h1 class="titre"> Pose de chaudières et poêles granulés de bois </h1>
+          <p class="text">
+            Artisan depuis 2001, je vous propose mes services pour la pose de chaudières à granulés de bois.
+            Au départ, installé en entreprise individuelle, puis en EURL sous le nom de <a class="lien" href="http://www.technichauf.fr/" target="_blank">TECHNICHAUF</a>, j'ai installé
+            mes premières chaudières à granulés de bois en 2005. <br/>
+            Etant seul dans l'entreprise, vous m'aurez comme seul interlocuteur, que ce soit pour le coté commercial,
+            par l'installation des chaudières, ou pour les entretiens et le SAV. <br/>
+            Ayant passé mes qualifications Qualibois depuis des années, je suis aujourd'hui RGE bois pour que vous puissiez obtenir les aides financières du moment. <br/>
+            J'installe également des poêles et cuisinières à granulés hydraulique, des chaudières à bois déchiqueté ou bois buche. <br/>
+            Je travaille avec des marques et distributeurs sérieux qui ont pignon sur rue depuis de nombreuses années. Toutes ces marques ont des techniciens et des dépôts de pièces détachées dans les environs (Villemoirieu (38), Pontcharra (38) ou St Badolph (73)).
+          </p>
+        </article>
+      </div>
+
+      <div class="decoration">
+            <!-- Ce div n'est là que pour les bordures-->
+      </div>
+
+      <div class="container">
+        <article>
+          <img width="23.5%" src="Images/Okofën.JPG">
+          <img class="imagesGalerie" src="Images/chaudiereBissuel.JPG">
+          <img class="imagesGalerie" src="Images/chaudMaison.JPG">
+          <img class="imagesGalerie" src="Images/smart80.JPG">
+
+          <p class="text2">
+            Voici les liens vers les sites de mes fournisseurs:
+            <a class="lien" target="_blank" href="https://www.oekofen.com/fr-fr/chaudiere-a-pellets-oekofen/">Ökofen</a>,
+            <a class="lien" target="_blank" href="http://www.labelenergie.com/"> Label energie</a>,
+            <a class="lien" target="_blank" href="https://www.laudevco.com/"> Laudevco</a>,  <br/>
+            Et pour les
+            <a class="lien" target="_blank" href="https://www.oekofen.com/fr-fr/prime-coupe-de-pouce"> aides</a>.
+          </p>
+        </article>
+      </div>
+
+      <div class="decoration">
+        <!-- Autre bordure -->
+      </div>
+
+      <footer>
+        <div class="elementsGras"> TECHNI'CHAUF </div>
+        LES AVENIERES <br/>
+        <div class="elementsGras"> Tel: O6.82.58.98.80 </div>
+      </footer>
+
+    </section>
+  </body>
+</html>
